a2e145c8516326f15f3690319e10c66f5627be64
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-10-29 04:00:26 -0400

    done

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import imagesCard from './imagesCard.hbs';
 import API from './fetchImages';
-import fetchImages from './fetchImages';
 
 const lightbox = new SimpleLightbox('.photo-card a', {
   captions: true,
@@ -18,11 +17,14 @@ const refs = {
   form: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
   loadMore: document.querySelector('.load-more'),
+  guard: document.querySelector('.guard'),
 };
 
-refs.form.addEventListener('submit', onSearch);
-
 let searchQuery = '';
+let page = 1;
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
 
 async function onSearch(event) {
   try {
@@ -31,6 +33,9 @@ async function onSearch(event) {
     page = 1;
 
     const response = await API.fetchImages(searchQuery, page);
+    cleanGallery();
+    createGallery(response);
+    lightbox.refresh();
 
     if (searchQuery.trim() === '') {
       Notify.failure(
@@ -42,19 +47,14 @@ async function onSearch(event) {
 
     if (response.totalHits > 0) {
       Notify.success(`Hooray! We found ${response.totalHits} images.`);
+      window.scrollTo(0, 0);
     }
 
-    /* ------------- Button "LoadMore"-------------- */
-    // if (response.hits.length === 40) {
-    //   refs.loadMore.removeAttribute('hidden');
-    // } else {
-    //   refs.loadMore.setAttribute('hidden', true);
-    // }
-
-    observer.observe(guard);
-    cleanGallery();
-    createGallery(response);
-    lightbox.refresh();
+    if (response.hits.length === 40) {
+      refs.loadMore.removeAttribute('hidden');
+    } else {
+      refs.loadMore.setAttribute('hidden', true);
+    }
   } catch (error) {
     console.error(error);
   }
@@ -77,65 +77,32 @@ function cleanGallery() {
   refs.gallery.innerHTML = '';
 }
 
-/* ------------- Button "LoadMore"-------------- */
+function pageScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
 
-// refs.loadMore.addEventListener('click', onLoadMore);
-// let page = 1;
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
 
-// async function onLoadMore() {
-//   try {
-//     page += 1;
-//     const response = await API.fetchImages(searchQuery, page);
+async function onLoadMore() {
+  try {
+    page += 1;
+    const response = await API.fetchImages(searchQuery, page);
+    const totalPages = Math.ceil(response.totalHits / 40);
 
-//     createGallery(response);
-//     lightbox.refresh();
+    createGallery(response);
+    lightbox.refresh();
+    pageScroll();
 
-//     const totalPages = Math.ceil(response.totalHits / 40);
-
-//     if (page === totalPages) {
-//       Notify.info("We're sorry, but you've reached the end of search results");
-//       refs.loadMore.setAttribute('hidden', true);
-//     }
-
-//     const { height: cardHeight } = document
-//       .querySelector('.gallery')
-//       .firstElementChild.getBoundingClientRect();
-
-//     window.scrollBy({
-//       top: cardHeight * 1,
-//       behavior: 'smooth',
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-/* ------------- Intersection Observer -------------- */
-
-const guard = document.querySelector('.guard');
-
-const options = {
-  root: null,
-  rootMargin: '50px',
-  threshold: 1,
-};
-
-const observer = new IntersectionObserver(onScroll, options);
-
-let page = 1;
-
-function onScroll(entries) {
-  const totalPages = Math.ceil(response.totalHits / 40);
-
-  entries.forEach(entry => {
-    console.dir(entry);
-    if (entry.isIntersecting) {
-      page += 1;
-      API.fetchImages(searchQuery, page).createGallery(response);
-    }
     if (page === totalPages) {
       Notify.info("We're sorry, but you've reached the end of search results");
-      observer.unobserve(guard);
+      refs.loadMore.setAttribute('hidden', true);
     }
-  });
+  } catch (error) {
+    console.error(error);
+  }
 }
